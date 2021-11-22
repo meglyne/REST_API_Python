4e65495d0251713993d1244356035bb0dc07b098
Margot Marc <margot.marc@outlook.fr>
2021-11-22 11:45:21 +0100

    Added test: POST returns JSON parsable response

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class TestAPIService:
 
@@ -6,14 +7,13 @@ class TestAPIService:
         r = requests.get('http://localhost:5000')
         assert r.status_code == 200
 
-    def test_post_request_code_200(self):
+    def test_post_request_returns_code_200(self):
         message = 'This is a test message !'
         r = requests.post('http://localhost:5000', data=message)
         assert r.status_code == 200
 
-class TestIncDec:
-    def test_increments(self):
-        assert inc_dec.increment(3) == 4
-
-    def test_decrement(self):
-        assert inc_dec.decrement(3) == 2
+    def test_post_request_returns_json_response(self):
+        message = 'This is another test message !'
+        r = requests.post('http://localhost:5000', data=message)
+        data = json.loads(r.text)
+        assert data["status"] == 200

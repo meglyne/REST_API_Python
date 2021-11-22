f54c9ba74c69d2604102a95f293c8779bf877272
Margot Marc <margot.marc@outlook.fr>
2021-11-22 11:45:21 +0100

    API now returns message_id for POST on /

--- app/messaging_api.py
+++ app/messaging_api.py
@@ -7,7 +7,7 @@ def create_app():
     @app.route("/", methods=['GET', 'POST'])
     def post_message():
         if request.method == 'POST':
-            response = make_response({'status': 200}, 200)
+            response = make_response({'status': 200, 'message_id': 0}, 200)
             return response
         elif request.method == 'GET':
             response = make_response('', 200)

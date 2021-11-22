926005d5ae14a1a4047b721ecdfe53789204db3b
Margot Marc <margot.marc@outlook.fr>
2021-11-22 13:23:40 +0100

    API returns different message_id

--- app/messaging_api.py
+++ app/messaging_api.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask import request, make_response
+from multiprocessing import Value
 
 def create_app():
+    #Creating a variable shared across Flask processes
+    message_id = Value('i', 0)
     app = Flask(__name__)
 
     @app.route("/", methods=['GET', 'POST'])
     def post_message():
         if request.method == 'POST':
-            response = make_response({'status': 200, 'message_id': 0}, 200)
-            return response
+            with message_id.get_lock():
+                response = make_response({'status': 200, 'message_id': message_id.value}, 200)
+                message_id.value +=1
+                return response
         elif request.method == 'GET':
             response = make_response('', 200)
             return response

e579903e334bec8ce2f0d97fd700ce1012ca4142
Margot Marc <margot.marc@outlook.fr>
2021-11-22 11:45:21 +0100

    API now returns code 200 for POST on /

--- app/messaging_api.py
+++ app/messaging_api.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import request, make_response
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
-    def hello_world():
-        return "<p>Hello, World!</p>"
+    @app.route("/", methods=['GET', 'POST'])
+    def post_message():
+        if request.method == 'POST':
+            response = make_response('', 200)
+            return response
+        elif request.method == 'GET':
+            response = make_response('', 200)
+            return response
 
     return app

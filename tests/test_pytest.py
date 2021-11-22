2a97d379f4a8a0107949731ae1582ca479e80a2d
Margot Marc <margot.marc@outlook.fr>
2021-11-22 21:17:36 +0100

    Updated tests to reflect eventual env changes

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,8 +1,9 @@
 # tests/test_pytest.py
 
-import redis
+from redis import Redis
 import requests
 import json
+from os import environ
 
 import pytest
 
@@ -53,9 +54,17 @@ class TestDatabaseService:
 
     @pytest.fixture(scope="module")
     def redis_connection(self):
-        return redis.Redis(host='localhost', port=6379, db=0)
+        r = None
+        FLASK_ENV=environ.get('FLASK_ENV')
+        if FLASK_ENV == "production":
+            #using hostname as host to ease connection since ip might be unknown
+            r = Redis(host="redis-server", port=6379, db=0, socket_connect_timeout=2, socket_timeout=2)
+        else:
+            r = Redis(host="redis-server-dev", port=6379, db=0, socket_connect_timeout=2, socket_timeout=2)
+        return r
 
     def test_service_is_available(self, redis_connection):
+        print(redis_connection)
         assert redis_connection.ping() == True
 
         

ee983a7f25b7f347dd24e10928558692cc8f97b2
Margot Marc <margot.marc@outlook.fr>
2021-11-22 11:45:21 +0100

    Added test: POST request returns code 200

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,17 +1,14 @@
-from inc_dec import inc_dec
-from app import messaging_api
 import requests
-import pytest
 
 class TestAPIService:
-    @pytest.fixture
-    def start_service(self):
-        app = messaging_api.create_app()
-        with app.test_client() as client:
-            yield client
 
-    def test_service_is_available(self, start_service):
+    def test_service_is_available(self):
         r = requests.get('http://localhost:5000')
+        assert r.status_code == 200
+
+    def test_post_request_code_200(self):
+        message = 'This is a test message !'
+        r = requests.post('http://localhost:5000', data=message)
         assert r.status_code == 200
 
 class TestIncDec:

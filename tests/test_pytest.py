0100456e3c719ec84f99235a57d1f603ef59ac0d
Margot Marc <margot.marc@outlook.fr>
2021-11-23 14:29:15 +0100

    Added test: 400 w/o JSON. Refactored tests w/ JSON

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -13,38 +13,46 @@ class TestAPIService:
         r = requests.get('http://localhost:5000')
         assert r.status_code == 200
 
-    def test_post_request_returns_code_200(self):
-        message = 'This is a test message !'
-        r = requests.post('http://localhost:5000', data=message)
+    def test_post_request_with_json_returns_code_200(self):
+        payload = {'message': 'test'}
+        r = requests.post('http://localhost:5000', json=payload)
         assert r.status_code == 200
 
-    def test_post_request_returns_json_response(self):
-        message = 'This is another test message !'
+    def test_post_request_without_json_returns_code_400(self):
+        message = 'This is a test string !'
         r = requests.post('http://localhost:5000', data=message)
+        assert r.status_code == 400
+
+
+    def test_post_request_returns_json_response(self):
+        payload = {'message':'This is another test message !'}
+        r = requests.post('http://localhost:5000', json=payload)
         # testing for json formating by loading response text
         data = json.loads(r.text)
         assert data["status"] == 200
 
     def test_post_request_returns_message_id(self):
-        message = 'Yet another test message !'
-        r = requests.post('http://localhost:5000', data=message)
+        # Tests that a POST request returns a JSON response with "message_id" as one
+        # of its keys
+        payload = {'message':'Yet another test message !'}
+        r = requests.post('http://localhost:5000', json=payload)
         data = json.loads(r.text)
         # asserting for message_id key in data
         assert "message_id" in data
 
     def test_post_returns_different_message_id(self):
-        message1 = 'test1'
-        message2 = 'test2'
-        r1 = requests.post('http://localhost:5000', data=message1)
-        r2 = requests.post('http://localhost:5000', data=message2)
+        payload1 = {'message':'test1'}
+        payload2 = {'message':'test2'}
+        r1 = requests.post('http://localhost:5000', json=payload1)
+        r2 = requests.post('http://localhost:5000', json=payload2)
         data1 = json.loads(r1.text)
         data2 = json.loads(r2.text)
         assert data1["message_id"] != data2["message_id"]
     
     def test_message_posted_is_accessible(self):
-        message = 'test'
+        payload = {'message':'test'}
         base_url = 'http://localhost:5000'
-        post_r = requests.post(base_url, data=message)
+        post_r = requests.post(base_url, json=payload)
         data = json.loads(post_r.text)
         message_link = '{base_url}/msg/{message_id}'.format(base_url=base_url, message_id=data["message_id"])
         get_message_r = requests.get(message_link)
